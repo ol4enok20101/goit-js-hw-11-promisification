685e800452fd2af11889030542705a8bc807039a
Olga Uzun <67106495+ol4enok20101@users.noreply.github.com>
2021-01-19 17:31:10 +0200

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,24 @@
+// Задание 1
+// Напиши функцию delay(ms), которая возвращает промис, переходящий в состояние "resolved" через ms миллисекунд.
+// Значением исполнившегося промиса должно быть то кол - во миллисекунд которое передали во время вызова функции delay.
+
+const delay = (ms) => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      const success = console.log(ms);
+
+      if (success) {
+        resolve(console.log("успех"));
+      }
+    }, ms);
+  });
+};
+
+const logger = (time) => console.log(`Resolved after ${time}ms`);
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
+
+// То, что возвращает коллбек, это будет результатом промиса, который возвращает then
